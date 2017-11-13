712add97be01aa5937cd44267455f25c5212fb5e
oliegjio <oliegjio@gmail.com>
2017-11-13 16:10:05 +0300

    Small changes: output IP, camera device

--- backend/main.cpp
+++ backend/main.cpp
@@ -9,24 +9,19 @@ int main(int argc, char** argv)
 {
   libvlc_instance_t* vlc;
   const char* url;
-  
-  const char* sout = "#transcode{vcodec=mjpg, vb=1600}:http{dst=127.0.0.1:8090/cam.mp4}";
-  const char* media_name = "Foo";
+  const char* sout = "#transcode{vcodec=mjpg, vb=1600}:http{dst=192.168.1.156:8080/camera.mp4}";
+  const char* media_name = "stream1";
 
-  url = "v4l2:///dev/video1";
+  url = "v4l2:///dev/video0";
 
   vlc = libvlc_new(0, NULL);
   libvlc_vlm_add_broadcast(vlc, media_name, url, sout, 0, NULL, true, false);
   libvlc_vlm_play_media(vlc, media_name);
 
-  sleep(60);
+  sleep(60 * 60 * 12);
 
   libvlc_vlm_stop_media(vlc, media_name);
   libvlc_vlm_release(vlc);
-
-  return 0;
-
-
 
   return 0;
 }
